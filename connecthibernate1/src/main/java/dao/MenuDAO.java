c62b7f78fcfb03f823412f717ae8f149a1851547
huudiep520 <huudiep520@gmail.com>
2016-04-06 21:01:37 +0700

    tạo MenuDAO

---
+++ connecthibernate1/src/main/java/dao/MenuDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MenuDAO {
+	public void InsertInto(){
+		Configuration con = new Configuration();
+		con.configure("hibernate.cfg.xml");
+		SessionFactory sf = 
+	}
+}
